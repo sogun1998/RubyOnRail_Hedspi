d60888d896a5c4b5bbba0dce8d1c889878262d05
sogun1998 <nguyenducanh26898@gmail.com>
2019-09-15 12:05:40 +0700

    Update w1 ver 2

--- w1/ptBac2.rb
+++ w1/ptBac2.rb
@@ -1,3 +1,4 @@
+require 'cmath'
 puts "Nhap tham so A:"
 a = gets()
 a = a.to_i
@@ -7,3 +8,21 @@ b= b.to_i
 puts "Nhap tham so C:"
 c = gets()
 c = c.to_i
+if a != 0 then
+    tmp = (b*b) - (4*a*c)
+    delta = CMath.sqrt(tmp)
+    if delta > 0 then
+      x1 = (-b + delta)/(2*a)
+      x2 = (-b - delta)/(2*a)
+      puts "x1: #{x1} - x2: #{x2}"
+    else
+      if tmp == 0 then
+        x = -b/(2*a)
+        puts "Ta co nghiệm kép x1 = x2 = : #{x}"
+      else
+        puts "Phuong trinh vô nghiệm"
+      end
+    end
+else
+    puts "Ko phải phuong trinh bac 2"
+  end
