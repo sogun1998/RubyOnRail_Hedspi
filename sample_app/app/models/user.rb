b149c16d8a58cbf897c3ecaa26e8255e5d4e3a28
sogun1998 <nguyenducanh26898@gmail.com>
2019-11-25 12:25:05 +0000

    Finish user edit, update, index, and destroy actions

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    
     attr_accessor :remember_token
     before_save { email.downcase! }
     validates :name,  presence: true, length: { maximum: 50 }
@@ -7,7 +8,7 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
     has_secure_password
-    validates :password, presence: true, length: { minimum: 6 }
+    validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
     def User.digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
@@ -29,4 +30,5 @@ class User < ApplicationRecord
     def forget
         update_attribute(:remember_digest, nil)
     end
+    
 end
